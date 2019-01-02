0910f1a4eae7ab1697d31d1c6a77512f2ed9312d
Mauricio Feldman-Abe <mauabe@gmx.com>
2019-01-02 11:28:22 -0800

    Escape characters in python

---
+++ ex10.py
@@ -0,0 +1,44 @@
+print ("I am 6'3\" tall.")
+print ('I am 6\'3" tall.')
+print("I want to escape double back-slaches: \\")
+
+tabby_cat = "\tI'm tabbed in/"
+persian_cat = "I'm split \non a line."
+backslash_cat = "I'm \\a \\cat."
+
+fat_cat = """
+I'll do a list:
+\t* Cat food
+\t* Fishies
+\t* Catnip\n\t* Grass
+"""
+
+print (tabby_cat)
+print (persian_cat)
+print (backslash_cat)
+print (fat_cat)
+
+#Escape Sequences
+
+# print(
+# \n '\\ continuation character'
+# \n \'  
+# \n\" 
+# \n '\a  ASCII bell (BEL)'
+# \n\b 'backspace' 
+# \n\f 'ascii formfeed(FF)'
+# \n \n 'linefeed (LF)'
+# \n \N{name} 'Chracter named name (Unicode oly)'
+# \n \r 'ASCII carriage return (CR)'
+# \n\t 'ASCII horizontal tab'
+# \n\uxxxx 'ASCII 16-bit hex (Unicode only)'
+# \n\Uxxxxxxxx 'ASCII32-bit hex (Unicode only)'
+# \n \v 'ASCII Vertical tab'
+# \n\ooo 'Character with octal value oo'
+# \n\xhh  'Character with hex valie hh'
+# )
+
+while True:
+for i in ["/","-","|","\\","|"]:
+            print "%s\r" % i,
+
