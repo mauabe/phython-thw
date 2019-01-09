4b38d8f64613b918747f4d2905762f1293d81253
Mauricio Feldman-Abe <6901736+mauabe@users.noreply.github.com>
2019-01-08 21:07:02 -0800

    functions return something

---
+++ ex22.py
@@ -0,0 +1,19 @@
+def add(a, b):
+  print("adding %d + %d" %(a, b))
+  return a + b
+
+def subtract(a, b):
+  print("Subtracting %d - %d" % (a, b))
+  return a - b
+
+def multiply(a, b):
+  print("Multiplying %d * %d" % (a, b))
+  return a * b
+
+def divide (a, b):
+  print("Dividing %d / %d" % (a, b))
+  return a / b
+
+
+print ("Let's do some math with just suing functions")
+
