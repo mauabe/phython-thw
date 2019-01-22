aefdae7803bdfd1863a64b6f64dcf9760e871237
Mauricio Feldman-Abe <mauabe@gmx.com>
2019-01-22 15:00:04 -0800

    booleans

---
+++ ex28.py
@@ -0,0 +1,47 @@
+#PRACTICE this:
+
+#NOT
+!false
+!true 
+
+#OR
+true or false 
+true or true  
+false or true 
+false or false 
+
+#AND
+true and false 
+true and true 
+false and true  
+false and false 
+
+#NOT OR
+not(true or false) 
+not(true or true) 
+not(false or true) 
+not(false or false) 
+
+#NOT AND
+not(true and false) 
+not(true and true) 
+not(false and true) 
+not(false and false) 
+
+#!=
+1 != 0 
+1 != 1 
+0 != 1 
+0 != 0 
+
+#==
+1 == 0 
+1 ==1  
+0 == 1 
+0 == 0 
+
+
+True and True
+False and True
+1 == 1 and 2 == 1
+"test" == "test”
