b102811c955718428b184d75fe62f1437ac09ac1
Mauricio Feldman-Abe <mauabe@gmx.com>
2019-01-24 18:15:35 -0800

    for loop

--- ex31.py
+++ ex31.py
@@ -18,4 +18,15 @@ if bear == '1':
 
 elif door == '2':
   print('You stare into the endless abyss at Cthulhu\'s retina')
-  
+  print('1. Blueberries')
+  print('2. Yellow jacket clothespins.')
+  print('3. Undestanding revolvers yelling melodies.'
+
+  insanity = input('>')
+
+  if insanity == '1' or insanity == '2':
+      print('Your body survives powered by a mind of jello. Good job!')
+  else:
+      print ('The insanity rots your eyes into a pool of muck. Good job!')
+else:
+      print ('You stumble around and fall on a knife and die. Good job!')
